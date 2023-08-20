9a9d673818ae4436f17aec480351f9b583644f67
Aryan Sharma <99004151+arsh8254@users.noreply.github.com>
2023-08-21 03:19:50 +0530

    Add files via upload

---
+++ Dice_Project/index.js
@@ -0,0 +1,24 @@
+// generating random number from 1-6
+var randomNumber1  = Math.floor(Math.random() * 6) + 1;
+
+// adding dice + above number + .png to make address of image
+var randomDiceImage = "dice" + randomNumber1 + ".png";       // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;         // images/dice1.png - images/dice6.png
+
+// for player-1 image
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// For player-2 Image
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";   // images/dice1.png - images/dice6.png
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+} else { 
+    document.querySelector("h1").innerHTML="Oh, It's a Draw!";
+}
